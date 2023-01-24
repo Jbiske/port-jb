d6b2770c5ecdd2b00d2f816f7e672c879574ef45
Josh <biscuitemail2@gmail.com>
2023-01-24 01:55:17 -0500

    cool

--- src/pages/About.js
+++ src/pages/About.js
@@ -12,8 +12,7 @@ export default function About() {
     <h1>My Name is Joshua</h1>
 
   
-    <p>I am a 21 year old Web Developer who is currently looking for work. I have a certificate of Full Stack Web-Development from Michigan State Univeristy.
-        I love working with tech and am motivated to get to work. I have a high motor and a willingness to learn. I am a huge sports fan and love video games. Check out my reseme and current projects.
+    <p>I am a 21 year old Web Developer from Macomb, Michigan who is currently looking for oppurtunity to begin my career in Web Devlopment.
 
     </p>
     </div>
